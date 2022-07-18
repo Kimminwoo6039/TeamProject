beb068e2c7bd355ab8a94a32868a496e865cc1a0
minwoo <95rlaalsdn@daum.net>
2022-07-18 19:39:20 +0900

    .

--- Ims/src/main/java/com/Ims/shop/service/EmailService.java
+++ Ims/src/main/java/com/Ims/shop/service/EmailService.java
@@ -36,19 +36,19 @@ public class EmailService {
 			msg.addFrom(new InternetAddress[] {
 					new InternetAddress("######","I`ms")
 			});
-<<<<<<< HEAD
-			msg.setSubject("ì œëª©");
-			msg.setText("ë‚´ìš©");
-=======
-			msg.setSubject("Å×½ºÆ® ¸ŞÀÏ Á¦¸ñ"); //Á¦¸ñ..
-			msg.setText("Å×½ºÆ®ÀÔ´Ï´Ù"); // ÅØ½ºÆ® ³»¿ë..
->>>>>>> branch 'master' of https://github.com/gogogo1123/shopping.git
+
+			msg.setSubject("ï¿½ì £ï§ï¿½");
+			msg.setText("ï¿½ê¶¡ï¿½ìŠœ");
+
+			msg.setSubject("í…ŒìŠ¤íŠ¸ ë©”ì¼ ì œëª©"); //ì œëª©..
+			msg.setText("í…ŒìŠ¤íŠ¸ì…ë‹ˆë‹¤"); // í…ìŠ¤íŠ¸ ë‚´ìš©..
+
 			mailSender.send(msg);
 			
 			
 		} catch (Exception e) {
                   e.printStackTrace();
-                  System.out.println("ì „ì†¡ì‹¤íŒ¨");
+                  System.out.println("ï¿½ìŸ¾ï¿½ë„šï¿½ë–ï¿½ë™£");
 		}
 		
 		
